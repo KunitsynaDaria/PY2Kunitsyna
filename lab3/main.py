c25c0a0b60bad379a9e998f498a4674ee27d3906
Darya <115484233+KunitsynaDaria@users.noreply.github.com>
2023-01-28 00:34:32 +0300

    Update main.py

--- lab3/main.py
+++ lab3/main.py
@@ -11,11 +11,11 @@ class Book:
         return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
 
     @property
-    def name_(self) -> str:
+    def name(self) -> str:
         return self.name
 
     @property
-    def author_(self) -> str:
+    def author(self) -> str:
         return self.author
 
 
@@ -28,17 +28,17 @@ class PaperBook(Book):
         return f"Книга {self.name}. Автор {self.author}. Количество страниц {self.pages}"
 
     @property
-    def pages_(self) -> int:
-        return self.pages
+    def pages(self) -> int:
+        return self._pages
 
-    @pages_.setter
-    def pages_(self, pages: int) -> None:
+    @pages.setter
+    def pages(self, pages: int) -> None:
         """Устанавливает количество страниц в книге."""
         if not isinstance(pages, int):
             raise TypeError("Количество страниц должно быть типа int")
         if pages <= 0:
             raise ValueError("Количество страниц должно быть положительным числом")
-        self.pages = pages
+        self._pages = pages
 
 
 class AudioBook(Book):
@@ -50,14 +50,14 @@ class AudioBook(Book):
         return f"Книга {self.name}. Автор {self.author}. Продолжительность {self.duration}"
 
     @property
-    def duration_(self) -> float:
-        return self.duration
+    def duration(self) -> float:
+        return self._duration
 
-    @duration_.setter
-    def duration_(self, duration: float) -> None:
+    @duration.setter
+    def duration(self, duration: float) -> None:
         """Устанавливает продолжительность книги."""
         if not isinstance(duration, float):
             raise TypeError("Продолжительность должна быть типа float")
         if duration <= 0:
             raise ValueError("Продолжительность должна быть положительным числом")
-        self.duration = duration
+        self._duration = duration
